cff1218514e6f25f2e4ed504703abd88e6df669b
khondokerfahad <rockingfahad02@gmail.com>
2026-04-13 16:40:12 +0600

    13-04-2026

--- Playwright/practice_project/first_test.py
+++ Playwright/practice_project/first_test.py
@@ -1,9 +1,8 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.firefox.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     page.goto('https://google.com')
-    print(f"Page title : {page.title()}")
+    print(page.title())
     browser.close()
-
